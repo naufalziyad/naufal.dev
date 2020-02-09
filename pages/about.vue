fd70d1c9c26d233a723345026fe4535aea564439
Naufal Ziyad <naufal.ziyad@detik.com>
2020-02-09 15:31:38 +0700

    add: Fundamental Nuxt.JS

---
+++ pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>This is spartan !!</h2>
+  </div>
+</template>
+
+
+<style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+body {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+h2 {
+  font-size: 48px;
+  color: #389acd;
+}
+p {
+  font-size: 18px;
+  color: #ababab;
+}
+</style>
